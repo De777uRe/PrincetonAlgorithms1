acee0678ef0b708c416cc33e093460dfc6caac42
William Goins <de777urest@gmail.com>
2018-02-24 17:58:52 -0600

    Completed assignment, needs cleaning up

---
+++ src/PercolationStats.java
@@ -0,0 +1,50 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+	
+	private int numTrials = 0;
+	private double thresholds[];
+	
+	public PercolationStats(int size, int trials) {
+		numTrials = trials;
+		thresholds = new double[trials];
+		
+		for (int i = 0; i < trials; i++) {
+			Percolation percSim = new Percolation(size);
+			percSim.runSim();
+			
+			thresholds[i] = percSim.percolationThreshold();
+		}
+		
+		StdOut.println("mean\t\t\t = " + mean());
+		StdOut.println("stddev\t\t\t = " + stddev());
+		StdOut.println("95% confidence interval\t = [" + confidenceLo() + ", " + confidenceHi() + "]");
+	}
+	
+	public double mean() {
+		return StdStats.mean(thresholds);
+	}
+	
+	public double stddev() {
+		return StdStats.stddevp(thresholds);
+	}
+	
+	public double confidenceLo() {
+		return mean() - ((1.96 * stddev()) / Math.sqrt(numTrials));
+	}
+	
+	public double confidenceHi() {
+		return mean() + ((1.96 * stddev()) / Math.sqrt(numTrials));
+	}
+	
+	public static void main(String[] args) {
+		StdOut.println("Enter size of grid: ");
+		int size = StdIn.readInt();
+		StdOut.println("Enter number of trials: " );
+		int trials = StdIn.readInt();
+		
+		PercolationStats statSim = new PercolationStats(size, trials);
+	}
+}
