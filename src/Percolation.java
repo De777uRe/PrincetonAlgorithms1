2b29fc0aecfd5709fc92dbb54de6babfef4fb09b
William Goins <de777urest@gmail.com>
2018-02-25 22:30:33 -0600

    Passing grade...?

--- src/Percolation.java
+++ src/Percolation.java
@@ -4,168 +4,178 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-	
-	private static int VIRTUAL_TOP;
-	private static int VIRTUAL_BOT;
-	
-	private static int size;
-	private int openSites = 0;
-	private int[][] grid;
-	
-	private boolean percolates = false;
-	
-	private WeightedQuickUnionUF wqUn;
-	
-	public Percolation(int n) {
-		if (n <= 0) {
-			throw new IllegalArgumentException("Cannot instantiate model of size <= 0");
-		}
-		else {
-			size = n;
-			grid = new int[size][size];
-			
-			VIRTUAL_TOP = size * size;
-			VIRTUAL_BOT = size * size + 1;
-			
-			wqUn = new WeightedQuickUnionUF(size*size + 2);
-			
-			for (int i = 0; i < n; i++) {
-				wqUn.union(gridToWQUN(0, i), VIRTUAL_TOP);
-				wqUn.union(gridToWQUN(size-1, i), VIRTUAL_BOT);
-			}
-			
-			StdRandom.setSeed(System.currentTimeMillis());
-			
-			int openX = StdRandom.uniform(size) + 1;
-			int openY = StdRandom.uniform(size) + 1;
-			
-			while (!percolates) {
-				openX = StdRandom.uniform(size) + 1;
-				openY = StdRandom.uniform(size) + 1;
-				open(openX, openY);
-//				printGrid();
-			}
-			
-//			StdOut.println("System percolates!!!");
-		}
-	}
-	
-	public void open(int row, int col) {
-		if (grid[row - 1][col - 1] != 1)
-		{
-			grid[row - 1][col - 1] = 1;
-			openSites++;
-		}
-		
-		performUnions(row - 1, col - 1);
-	}
-	
-	public boolean isOpen(int row, int col) {
-		return grid[row - 1][col - 1] == 1;
-	}
-	
-	public boolean isFull(int row, int col) {
-		int wqunCoord = gridToWQUN(row-1, col-1);
-		return isOpen(row, col) && wqUn.connected(wqunCoord, VIRTUAL_TOP);
-	}
-	
-	public int numberOfOpenSites() {
-		return openSites;
-	}
-	
-	public boolean percolates() {
-		return wqUn.connected(VIRTUAL_TOP, VIRTUAL_BOT);
-	}
-	
-	private void performUnions(int row, int col) {
-		int up;
-		int down;
-		int left;
-		int right;
-		
-		int wqUnCoord = gridToWQUN(row, col);
-		
-		if (row != 0) {
-			if (grid[row - 1][col] == 1) {
-				up = gridToWQUN(row - 1, col);
-				wqUn.union(wqUnCoord, up);
-			}
-		}
-		
-		if (row != size - 1) {
-			if (grid[row + 1][col] == 1) {
-				down = gridToWQUN(row + 1, col);
-				wqUn.union(wqUnCoord, down);
-			}
-		}
-		
-		if (col != 0) {
-			if (grid[row][col - 1] == 1) {
-				left = gridToWQUN(row, col - 1);
-				wqUn.union(wqUnCoord, left);
-			}
-		}
-		
-		if (col != size - 1) {
-			if (grid[row][col + 1] == 1) {
-				right = gridToWQUN(row, col + 1);
-				wqUn.union(wqUnCoord, right);
-			}
-		}
-		
-		percolates = percolates();
-//		StdOut.println("Number of Components: " + wqUn.count());
-	}
-	
-	private void runSim() {
-		StdRandom.setSeed(System.currentTimeMillis());
-		
-		int openX = StdRandom.uniform(size) + 1;
-		int openY = StdRandom.uniform(size) + 1;
-		
-		while (!percolates) {
-			openX = StdRandom.uniform(size) + 1;
-			openY = StdRandom.uniform(size) + 1;
-			open(openX, openY);
-//			printGrid();
-		}
-	}
-	
-	public static void main(String[] args) {
-		StdOut.println("Specify size of grid: " );
-		int runSize = StdIn.readInt();
-		Percolation test = new Percolation(runSize);
-		StdRandom.setSeed(System.currentTimeMillis());
-		
-		while (!test.percolates) {
-			int openX = StdRandom.uniform(runSize) + 1;
-			int openY = StdRandom.uniform(runSize) + 1;
-			test.open(openX, openY);
-			test.printGrid();
 
-			if (test.percolates) {
-				StdOut.println("SYSTEM PERCOLATES!!!");
-				System.exit(0);
-			}
-		}
-	}
-	
-	private void printGrid() {
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				if (j < size - 1) {
-					StdOut.print(grid[i][j] + " ");
-				}
-				else {
-					StdOut.print(grid[i][j] + "\n");
-				}
-			}
-		}
-		StdOut.println();
-	}
-	
-	private static int gridToWQUN(int row, int col)
-	{
-		return size * row + col;
-	}
+    private int virtualTop;
+    private int virtualBot;
+
+    private int size;
+    private int openSites;
+    private boolean[][] grid;
+
+    private boolean percolates;
+
+    private WeightedQuickUnionUF wqUn;
+
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Cannot instantiate model of size <= 0");
+        } else {
+            size = n;
+            openSites = 0;
+            percolates = false;
+            grid = new boolean[size][size];
+            
+//            for (int i = 0; i < size; i++) {
+//                for (int j = 0; j < size; j++) {
+//                    grid[i][j] = false;
+//                }
+//            }
+
+            virtualTop = size * size;
+            virtualBot = size * size + 1;
+
+            wqUn = new WeightedQuickUnionUF(size * size + 2);
+
+            for (int i = 0; i < n; i++) {
+                wqUn.union(gridToWQUN(0, i), virtualTop);
+                wqUn.union(gridToWQUN(size - 1, i), virtualBot);
+            }
+
+//            int openX;
+//            int openY;
+//            
+//            while (!percolates) {
+//                openX = StdRandom.uniform(size) + 1;
+//                openY = StdRandom.uniform(size) + 1;
+//                open(openX, openY);
+//                 printGrid();
+//            }
+//
+//             StdOut.println("System percolates!!!");
+        }
+    }
+
+    public void open(int row, int col) {
+        if (row <= 0 || row > size || col <= 0 || col > size) {
+            throw new IllegalArgumentException("Input is outside grid range");
+        }
+        else {
+            if (!grid[row - 1][col - 1]) {
+                grid[row - 1][col - 1] = true;
+                openSites++;
+                
+                performUnions(row - 1, col - 1);
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row <= 0 || row > size || col <= 0 || col > size) {
+            throw new IllegalArgumentException("Input is outside grid range");
+        }
+        else {
+            return grid[row - 1][col - 1];
+        }
+    }
+
+    public boolean isFull(int row, int col) {
+        if (row <= 0 || row > size || col <= 0 || col > size) {
+            throw new IllegalArgumentException("Input is outside grid range");
+        }
+        else {
+            int wqunCoord = gridToWQUN(row - 1, col - 1);
+            return isOpen(row, col) && wqUn.connected(wqunCoord, virtualTop);
+        }
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    public boolean percolates() {
+        return wqUn.connected(virtualTop, virtualBot);
+    }
+
+    private void performUnions(int row, int col) {
+        int up;
+        int down;
+        int left;
+        int right;
+
+        int wqUnCoord = gridToWQUN(row, col);
+
+        if (row != 0) {
+            if (grid[row - 1][col]) {
+                up = gridToWQUN(row - 1, col);
+                wqUn.union(wqUnCoord, up);
+            }
+        }
+
+        if (row != size - 1) {
+            if (grid[row + 1][col]) {
+                down = gridToWQUN(row + 1, col);
+                wqUn.union(wqUnCoord, down);
+            }
+        }
+
+        if (col != 0) {
+            if (grid[row][col - 1]) {
+                left = gridToWQUN(row, col - 1);
+                wqUn.union(wqUnCoord, left);
+            }
+        }
+
+        if (col != size - 1) {
+            if (grid[row][col + 1]) {
+                right = gridToWQUN(row, col + 1);
+                wqUn.union(wqUnCoord, right);
+            }
+        }
+
+        percolates = percolates();
+    }
+
+    public static void main(String[] args) {
+        StdOut.println("Specify size of grid: ");
+        int runSize = StdIn.readInt();
+        Percolation test = new Percolation(runSize);
+
+        while (!test.percolates) {
+            int openX = StdRandom.uniform(runSize) + 1;
+            int openY = StdRandom.uniform(runSize) + 1;
+            test.open(openX, openY);
+            test.printGrid();
+
+            if (test.percolates) {
+//                StdOut.println("SYSTEM PERCOLATES!!!");
+            }
+        }
+    }
+
+    private void printGrid() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (j < size - 1) {
+                    if (grid[i][j] == true) {
+                        StdOut.print("1 ");
+                    }
+                    else {
+                        StdOut.print("0 ");
+                    }
+                } else {
+                    if (grid[i][j] == true) {
+                        StdOut.print("1\n");
+                    }
+                    else {
+                        StdOut.print("0\n");
+                    }
+                }
+            }
+        }
+        StdOut.println();
+    }
+
+    private int gridToWQUN(int row, int col) {
+        return size * row + col;
+    }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,49 +1,60 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-	
-	private int numTrials = 0;
-	private double thresholds[];
-	
-	public PercolationStats(int size, int trials) {
-		numTrials = trials;
-		thresholds = new double[trials];
-		
-		for (int i = 0; i < trials; i++) {
-			Percolation percSim = new Percolation(size);
-			
-			thresholds[i] = (double) percSim.numberOfOpenSites() / (double) (size * size);
-		}
-		
-		StdOut.println("mean\t\t\t = " + mean());
-		StdOut.println("stddev\t\t\t = " + stddev());
-		StdOut.println("95% confidence interval\t = [" + confidenceLo() + ", " + confidenceHi() + "]");
-	}
-	
-	public double mean() {
-		return StdStats.mean(thresholds);
-	}
-	
-	public double stddev() {
-		return StdStats.stddevp(thresholds);
-	}
-	
-	public double confidenceLo() {
-		return mean() - ((1.96 * stddev()) / Math.sqrt(numTrials));
-	}
-	
-	public double confidenceHi() {
-		return mean() + ((1.96 * stddev()) / Math.sqrt(numTrials));
-	}
-	
-	public static void main(String[] args) {
-		StdOut.println("Enter size of grid: ");
-		int size = StdIn.readInt();
-		StdOut.println("Enter number of trials: " );
-		int trials = StdIn.readInt();
-		
-		PercolationStats statSim = new PercolationStats(size, trials);
-	}
+
+    private int numTrials = 0;
+    private double[] thresholds;
+
+    public PercolationStats(int size, int trials) {
+        numTrials = trials;
+        thresholds = new double[trials];
+
+        for (int i = 0; i < trials; i++) {
+            Percolation percSim = new Percolation(size);
+            
+          int openX;
+          int openY;
+          
+          while (!percSim.percolates()) {
+              openX = StdRandom.uniform(size) + 1;
+              openY = StdRandom.uniform(size) + 1;
+              percSim.open(openX, openY);
+//               printGrid();
+          }
+
+            thresholds[i] = (double) percSim.numberOfOpenSites() / (double) (size * size);
+        }
+
+        StdOut.println("mean\t\t\t = " + mean());
+        StdOut.println("stddev\t\t\t = " + stddev());
+        StdOut.println("95% confidence interval\t = [" + confidenceLo() + ", " + confidenceHi() + "]");
+    }
+
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    public double stddev() {
+        return StdStats.stddevp(thresholds);
+    }
+
+    public double confidenceLo() {
+        return mean() - ((1.96 * stddev()) / Math.sqrt(numTrials));
+    }
+
+    public double confidenceHi() {
+        return mean() + ((1.96 * stddev()) / Math.sqrt(numTrials));
+    }
+
+    public static void main(String[] args) {
+        StdOut.println("Enter size of grid: ");
+        int size = StdIn.readInt();
+        StdOut.println("Enter number of trials: " );
+        int trials = StdIn.readInt();
+
+        PercolationStats statSim = new PercolationStats(size, trials);
+    }
 }
